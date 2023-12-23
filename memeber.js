7c3ba0f60b97ad2053c9c7d87ff38a347f466e92
Prashanth <prashanthcsgce@gmail.com>
2023-12-23 13:40:25 +0000

    Copilot second commit

---
+++ memeber.js
@@ -0,0 +1,30 @@
+function skillsMember() {
+    var skills = [
+        {
+            "name": "HTML",
+            "level": "80%"
+        },
+        {
+            "name": "CSS",
+            "level": "70%"
+        },
+        {
+            "name": "JavaScript",
+            "level": "60%"
+        },
+        {
+            "name": "PHP",
+            "level": "50%"
+        },
+        {
+            "name": "MySQL",
+            "level": "40%"
+        },
+        {
+            "name": "Java",
+            "level": "30%"
+        }
+    ];
+    return skills;
+}
+    
